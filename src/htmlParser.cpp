ade6f42a384bf917f38940114ce06ea2815c44ce
yk0112 <yosikou0726@gmail.com>
2023-10-08 14:00:04 +0900

    feat: 複数の属性値列をパースできるように変更

--- src/htmlParser.cpp
+++ src/htmlParser.cpp
@@ -1,34 +1,67 @@
+#include "./htmlParser.hpp"
+
 #include <boost/spirit/include/qi.hpp>
 #include <iostream>
 #include <string>
-#include "./htmlParser.hpp"
-
 
 namespace qi = boost::spirit::qi;
 
-Attribute parse_attribute(std::string input) {
+std::tuple<std::string, std::string> parse_attribute(std::string input) {
     auto begin = input.begin();
     auto end = input.end();
-Attribute output;
+    std::string attributeName;
+    std::string attributeValue;
 
     qi::rule<std::string::iterator, std::string()> attributeNameRule =
-        qi::lexeme[+(qi::char_ - qi::lit("="))];
+        +(qi::char_ - "=" - qi::space);
     qi::rule<std::string::iterator, std::string()> attributeValueRule =
-        qi::lexeme['"' >> +(qi::char_ - '"') >> '"'];
+        qi::lexeme['"' >> *(qi::char_ - '"') >> '"'];
 
     bool success = qi::phrase_parse(begin,
                                     end,
                                     attributeNameRule >> qi::lit("=") >> attributeValueRule,
                                     qi::space,
-                                    output.attributeName,
-                                    output.attributeValue);
+                                    attributeName,
+                                    attributeValue);
 
     if (success && begin == end) {
-        return output;
+        return std::make_tuple(attributeName, attributeValue);
+        // return std::make_tuple("test","foo bar");
     }
 
     std::cout << "fail to parse attribute \n";
     exit(0);
 }
 
+AttrMap attributes(std::string input) {
+    auto begin = input.begin();
+    auto end = input.end();
 
+    std::vector<std::string> attributes;
+    AttrMap result;
+
+    qi::rule<std::string::iterator, std::string()> attributeNameRule = +(qi::char_ - '=');
+    qi::rule<std::string::iterator, std::string()> attributeValueRule =
+        qi::lexeme[qi::char_('"') >> *(qi::char_ - '"') >> qi::char_('"')];
+
+    qi::rule<std::string::iterator, std::string()> attributePair =
+        attributeNameRule >> qi::char_("=") >> *qi::space >> attributeValueRule;
+
+    if (input.empty()) {
+        return result;
+    }
+
+    bool success = qi::parse(begin, end, attributePair % " ", attributes);
+
+    if (success && begin == end) {
+        for (auto &attr : attributes) {
+            auto name_and_value = parse_attribute(attr);
+            result.emplace(std::get<0>(name_and_value), std::get<1>(name_and_value));
+        }
+
+        return result;
+    }
+
+    std::cout << "fail to attributes \n";
+    exit(0);
+}
--- src/htmlParser.hpp
+++ src/htmlParser.hpp
@@ -1,10 +1,32 @@
 #include <string>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <memory>
 
-struct Attribute {
-    std::string attributeName;
-    std::string attributeValue;
+struct Element;
+
+using AttrMap = std::unordered_map<std::string, std::string>;
+
+enum class NodeType { Element, Text };
+
+struct Text {
+    std::string data;
 };
 
-Attribute parse_attribute(std::string input);
+struct Node {
+    NodeType node_type;
+    std::vector<std::unique_ptr<Node>> children;
+};
+
+struct Element {
+    std::string tag_name;
+    AttrMap attributes;
+};
 
 
+
+std::tuple<std::string, std::string> parse_attribute(std::string input);
+
+AttrMap attributes(std::string input);
