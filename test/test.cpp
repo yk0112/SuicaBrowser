458b2011ee9a3397922e435e35254dd4401eacce
yk0112 <yosikou0726@gmail.com>
2023-10-13 02:50:52 +0900

    test: タグ文字列のパース処理のテスト追加

--- test/test.cpp
+++ test/test.cpp
@@ -38,29 +38,66 @@ auto UnorderdMapEquals(const std::unordered_map<KeyType, ValueType> &expected)
     return UnorderdMapMatcher<KeyType, ValueType>(expected);
 }
 
-TEST_CASE("ParseAttribute", "[attribute]") {
-    auto result1 = parse_attribute("test=\"foobar\"");
+TEST_CASE("parse_attribute", "[attribute]") {
+    TagParser parser{};
+
+    auto result1 = parser.parse_attribute("test=\"foobar\"");
     REQUIRE(std::get<0>(result1) == "test");
     REQUIRE(std::get<1>(result1) == "foobar");
 
-    auto result2 = parse_attribute("test = \"foobar\"");
+    auto result2 = parser.parse_attribute("test = \"foobar\"");
     REQUIRE(std::get<0>(result2) == "test");
     REQUIRE(std::get<1>(result2) == "foobar");
+
+    auto result3 = parser.parse_attribute("test = \"\"");
+    REQUIRE(std::get<0>(result3) == "test");
+    REQUIRE(std::get<1>(result3) == "");
 
     // auto result3 = parse_attribute("test = \"foo bar\"");
     // REQUIRE(std::get<0>(result2) == "test");
     // REQUIRE(std::get<1>(result2) == "foo bar");
 }
 
-TEST_CASE("Attributes", "[attribute]") {
-    auto result1 = attributes("test=\"foobar\" abc=\"def\"");
+TEST_CASE("parse_attributes", "[attribute]") {
+    TagParser parser{};
+
+    auto result1 = parser.parse_attributes("test=\"foobar\" abc=\"def\"");
     std::unordered_map<std::string, std::string> expected1{{"test", "foobar"}, {"abc", "def"}};
     REQUIRE_THAT(result1, UnorderdMapEquals(expected1));
 
-    auto result2 = attributes("test = \"foobar\" abc = \"def\"");
+    auto result2 = parser.parse_attributes("test = \"foobar\" abc = \"def\"");
     REQUIRE_THAT(result2, UnorderdMapEquals(expected1));
 
-    auto result3 = attributes("");
+    auto result3 = parser.parse_attributes("");
     std::unordered_map<std::string, std::string> expected3{};
     REQUIRE_THAT(result3, UnorderdMapEquals(expected3));
+}
+
+TEST_CASE("open_tag", "[attribute]") {
+    TagParser parser{};
+
+    SECTION("tag without attributes") {
+        auto result = parser.open_tag("<p>aaaaaa");
+        std::unordered_map<std::string, std::string> expected{};
+        REQUIRE(result.first == "p");
+        REQUIRE_THAT(result.second, UnorderdMapEquals(expected));
+    }
+
+    SECTION("tag with a attribute") {
+        auto result = parser.open_tag("<p id=\"test\">");
+        std::unordered_map<std::string, std::string> expected{{"id", "test"}};
+        REQUIRE(result.first == "p");
+        REQUIRE_THAT(result.second, UnorderdMapEquals(expected));
+    }
+
+    SECTION("tag with multiple attributes") {
+        auto result = parser.open_tag("<p id=\"test\" class=\"sample\">");
+        std::unordered_map<std::string, std::string> expected{{"id", "test"}, {"class", "sample"}};
+        REQUIRE(result.first == "p");
+        REQUIRE_THAT(result.second, UnorderdMapEquals(expected));
+    }
+
+    SECTION("invalid tag") {
+        REQUIRE_THROWS(parser.open_tag("<p id>"));
+    }
 }
