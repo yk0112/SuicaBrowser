abd3f4a6ffc16b478a53bd0fed6b64d7180488ae
yk0112 <yosikou0726@gmail.com>
2023-11-10 16:19:16 +0900

    feat: CSSのパース処理追加

--- src/css/cssParser.cpp
+++ src/css/cssParser.cpp
@@ -8,31 +8,37 @@ namespace qi = boost::spirit::qi;
 
 using Rule = qi::rule<std::string::iterator, std::string()>;
 using Rule2 = qi::rule<std::string::iterator, Selector()>;
+using Rule3 = qi::rule<std::string::iterator, CSSRule()>;
 
 BOOST_PHOENIX_ADAPT_FUNCTION(Selector, capUniv, cap_univ, 1)
 BOOST_PHOENIX_ADAPT_FUNCTION(Selector, capClass, cap_class, 1)
 BOOST_PHOENIX_ADAPT_FUNCTION(Selector, capAttr, cap_attr, 4)
 BOOST_PHOENIX_ADAPT_FUNCTION(Selector, capType, cap_type, 1)
+BOOST_PHOENIX_ADAPT_FUNCTION(CSSRule, capCSSRule, cap_CSSRule, 1)
 
-Rule nameRule = +(qi::char_ - ':');
-Rule valueRule = +(qi::char_ - ';');
-Rule declarationRule = nameRule >> qi::char_(':') >> valueRule;
-Rule selectorRule = +(qi::char_ - ',');
-Rule opRule = qi::string("~=") | qi::char_('=');
+const Rule name = +(qi::char_ - ':');
+const Rule value = +(qi::char_ - ';');
+const Rule declaration = name >> qi::char_(':') >> value;
+const Rule selector = +(qi::char_ - ',');
+const Rule op = qi::string("~=") | qi::char_('=');
 
-Rule2 universalRule = qi::as_string[qi::char_('*')][qi::_val = capUniv(qi::_1)];
+const Rule2 universal_selector = qi::as_string[qi::char_('*')][qi::_val = capUniv(qi::_1)];
 
-Rule2 classRule = qi::lit('.') >> qi::as_string[+qi::char_][qi::_val = capClass(qi::_1)];
+const Rule2 class_selector = qi::lit('.') >> qi::as_string[+qi::char_][qi::_val = capClass(qi::_1)];
 
-Rule2 typeRule = qi::as_string[*(qi::char_)][qi::_val = capType(qi::_1)];
+const Rule2 type_selector = qi::as_string[*(qi::char_)][qi::_val = capType(qi::_1)];
 
-Rule2 attributeRule =
+const Rule2 attribute_selector =
     (qi::as_string[*(qi::char_ - '[' - ' ')] >> *qi::lit(' ') >> '[' >>
-     qi::as_string[*(qi::char_ - opRule - ' ')] >> *qi::lit(' ') >> qi::as_string[opRule] >>
+     qi::as_string[*(qi::char_ - op - ' ')] >> *qi::lit(' ') >> qi::as_string[op] >>
      *qi::lit(' ') >> qi::as_string[*(qi::char_ - ']' - ' ')] >> *qi::lit(' ') >>
      ']')[qi::_val = capAttr(qi::_1, qi::_2, qi::_3, qi::_4)];
 
-Rule2 selectorsRule = universalRule | classRule | attributeRule | typeRule;
+const Rule2 selectorsRule =
+    universal_selector | class_selector | attribute_selector | type_selector;
+
+const Rule3 cssRule = qi::as_string[+(qi::char_ - '{') >> qi::char_('{') >> *(qi::char_ - '}') >>
+                                    qi::char_('}')][qi::_val = capCSSRule(qi::_1)];
 
 Selector cap_univ(std::string s) {
     return Selector{Type::UniversalSelector, UniversalSelector{}};
@@ -56,7 +62,11 @@ Selector cap_attr(std::string s1, std::string s2, std::string s3, std::string s4
     }
 };
 
-Declaration declaration(std::string input) {
+CSSRule cap_CSSRule(std::string s1) {
+    return parse_rule(s1);
+};
+
+Declaration parse_declaration(std::string input) {
     auto begin = input.begin();
     auto end = input.end();
 
@@ -77,7 +87,7 @@ Declaration declaration(std::string input) {
     throw std::runtime_error("fail to declaration");
 }
 
-std::vector<Declaration> declarations(std::string input) {
+std::vector<Declaration> parse_declarations(std::string input) {
     auto begin = input.begin();
     auto end = input.end();
 
@@ -85,18 +95,18 @@ std::vector<Declaration> declarations(std::string input) {
     std::vector<Declaration> result{};
 
     bool sucsess =
-        qi::phrase_parse(begin, end, (declarationRule || *(qi::space)) % ';', qi::space, decls);
+        qi::phrase_parse(begin, end, (declaration || *(qi::space)) % ';', qi::space, decls);
 
     if (sucsess && begin == end) {
         for (auto &decl : decls) {
-            result.push_back(declaration(decl));
+            result.push_back(parse_declaration(decl));
         }
         return result;
     }
     throw std::runtime_error("fail to declarations");
 }
 
-std::vector<Selector> selectors(std::string input) {
+std::vector<Selector> parse_selectors(std::string input) {
     auto begin = input.begin();
     auto end = input.end();
 
@@ -126,9 +136,23 @@ CSSRule parse_rule(std::string input) {
                                     decls);
 
     if (sucsess && begin == end) {
-        return CSSRule{selectors(selecs), declarations(decls)};
+        return CSSRule{parse_selectors(selecs), parse_declarations(decls)};
     }
     throw std::runtime_error("fail to rule");
+}
+
+std::vector<CSSRule> parse(std::string input) {
+    auto begin = input.begin();
+    auto end = input.end();
+
+    std::vector<CSSRule> result{};
+
+    bool sucsess = qi::phrase_parse(begin, end, cssRule % *qi::space, qi::space, result);
+
+    if (sucsess && begin == end) {
+        return result;
+    }
+    throw std::runtime_error("fail to parse_rules");
 }
 
 } // namespace css
--- unit_test/css/test.cpp
+++ unit_test/css/test.cpp
@@ -94,22 +94,51 @@ struct SelectorsMatcher : Catch::Matchers::MatcherGenericBase {
     }
 
     std::string describe() const override {
-        return "declarations are equal";
+        return "Selectors are equal";
     }
 
   private:
     const std::vector<Selector> &expected_;
 };
 
+struct RulesMatcher : Catch::Matchers::MatcherGenericBase {
+    RulesMatcher(const std::vector<CSSRule> &expected) : expected_{expected} {}
+
+    bool match(const std::vector<CSSRule> &actual) const {
+
+        if (actual.size() != expected_.size()) {
+            return false;
+        }
+
+        for (int i = 0; i < actual.size(); i++) {
+            SelectorsMatcher selectorsMatcher{expected_[i].selectors};
+            DeclarationsMatcher declMatcher{expected_[i].declarations};
+
+            if (!selectorsMatcher.match(actual[i].selectors) ||
+                !declMatcher.match(actual[i].declarations))
+                return false;
+        }
+
+        return true;
+    }
+
+    std::string describe() const override {
+        return "Rules are equal";
+    }
+
+  private:
+    const std::vector<CSSRule> &expected_;
+};
+
 /////////////  TEST_CASE   /////////////////
 
 TEST_CASE("declarations", "[declarations]") {
 
-    auto result1 = declarations("foo : bar; piyo : piyopiyo;");
+    auto result1 = parse_declarations("foo : bar; piyo : piyopiyo;");
     std::vector<Declaration> expected{Declaration{"foo", "bar"}, Declaration{"piyo", "piyopiyo"}};
     REQUIRE_THAT(result1, DeclarationsMatcher{expected});
 
-    auto result2 = declarations("");
+    auto result2 = parse_declarations("");
     std::vector<Declaration> expected2{};
     REQUIRE_THAT(result2, DeclarationsMatcher{expected2});
 }
@@ -117,14 +146,14 @@ TEST_CASE("declarations", "[declarations]") {
 TEST_CASE("selector", "[selector]") {
 
     SECTION("universal selector") {
-        auto result = selectors("*");
+        auto result = parse_selectors("*");
         std::vector<Selector> expected{Selector{Type::UniversalSelector, UniversalSelector{}}};
 
         REQUIRE_THAT(result, SelectorsMatcher{expected});
     }
 
     SECTION("class selector") {
-        auto result = selectors(".test");
+        auto result = parse_selectors(".test");
 
         std::vector<Selector> expected{Selector{Type::ClassSelector, ClassSelector{"test"}}};
 
@@ -132,7 +161,7 @@ TEST_CASE("selector", "[selector]") {
     }
 
     SECTION("multiple selectors") {
-        auto result = selectors("test[foo=bar],a");
+        auto result = parse_selectors("test[foo=bar],a");
 
         auto content1 = AttributeSelector{"test", AttributeSelectorOp::eq, "foo", "bar"};
         auto content2 = TypeSelector{"a"};
@@ -143,7 +172,7 @@ TEST_CASE("selector", "[selector]") {
     }
 
     SECTION("multiple attributes") {
-        auto result = selectors("test [foo=bar], testtest[piyo~=guoo]");
+        auto result = parse_selectors("test [foo=bar], testtest[piyo~=guoo]");
 
         auto content1 = AttributeSelector{"test", AttributeSelectorOp::eq, "foo", "bar"};
         auto content2 = AttributeSelector{"testtest", AttributeSelectorOp::Contain, "piyo", "guoo"};
@@ -155,7 +184,7 @@ TEST_CASE("selector", "[selector]") {
     }
 }
 
-TEST_CASE("rule", "[rule]") {
+TEST_CASE("parse_rule", "[rule]") {
 
     SECTION("empty declarations") {
         auto result = parse_rule("test [foo=bar] {}");
@@ -194,5 +223,40 @@ TEST_CASE("rule", "[rule]") {
 
         REQUIRE_THAT(result.selectors, SelectorsMatcher{selecs});
         REQUIRE_THAT(result.declarations, DeclarationsMatcher{decls});
+    }
+}
+
+TEST_CASE("parse_rules", "[rule]") {
+
+    SECTION("single rule") {
+        auto result = parse("test [foo=bar] { aa: bb; cc: dd; }");
+
+        auto content1 = AttributeSelector{"test", AttributeSelectorOp::eq, "foo", "bar"};
+        std::vector<Selector> selecs1{Selector{Type::AttributeSelector, content1}};
+        std::vector<Declaration> decls1{Declaration{"aa", "bb"}, Declaration{"cc", "dd"}};
+        CSSRule rule1{selecs1, decls1};
+
+        std::vector<CSSRule> expected{rule1};
+
+        REQUIRE_THAT(result, RulesMatcher{expected});
+    }
+
+    SECTION("multiple rules") {
+        auto result = parse("test [foo=bar] { aa: bb; cc: dd; } \
+	                           test2[piyo = piyopiyo] { ee: ff; gg: hh; }");
+
+        auto content1 = AttributeSelector{"test", AttributeSelectorOp::eq, "foo", "bar"};
+        std::vector<Selector> selecs1{Selector{Type::AttributeSelector, content1}};
+        std::vector<Declaration> decls1{Declaration{"aa", "bb"}, Declaration{"cc", "dd"}};
+        CSSRule rule1{selecs1, decls1};
+
+        auto content2 = AttributeSelector{"test2", AttributeSelectorOp::eq, "piyo", "piyopiyo"};
+        std::vector<Selector> selecs2{Selector{Type::AttributeSelector, content2}};
+        std::vector<Declaration> decls2{Declaration{"ee", "ff"}, Declaration{"gg", "hh"}};
+        CSSRule rule2{selecs2, decls2};
+
+        std::vector<CSSRule> expected{rule1, rule2};
+
+        REQUIRE_THAT(result, RulesMatcher{expected});
     }
 }
