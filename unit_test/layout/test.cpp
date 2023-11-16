6e04bae8937c313d2b746c129c5a6d3be0f1effd
yk0112 <yosikou0726@gmail.com>
2023-11-17 02:08:57 +0900

    test: レンダリングツリー構築のテスト処理追加

--- unit_test/layout/test.cpp
+++ unit_test/layout/test.cpp
@@ -6,6 +6,42 @@ using namespace layout;
 
 //////////// CUSTOM MATCHR //////////////
 
+struct StyleNodeMatcher : Catch::Matchers::MatcherGenericBase {
+    StyleNodeMatcher(const std::shared_ptr<StyleNode> expected) : expected_{expected} {}
+
+    bool match(const std::shared_ptr<StyleNode> actual) const {
+        if (actual->node_type != expected_->node_type) {
+            return false;
+        }
+
+        if (actual->properties.size() != expected_->properties.size()) {
+            return false;
+        }
+
+        for (const auto &elem : expected_->properties) {
+            auto it = actual->properties.find(elem.first);
+            if (it == actual->properties.end() || it->second != elem.second) {
+                return false;
+            }
+        }
+
+        for (int i = 0; i < actual->children.size(); i++) {
+            StyleNodeMatcher snMatcher{expected_->children[i]};
+            if (!snMatcher.match(actual->children[i]))
+                return false;
+        }
+
+        return true;
+    }
+
+    std::string describe() const override {
+        return "StyleNodes are equal";
+    }
+
+  private:
+    const std::shared_ptr<StyleNode> expected_;
+};
+
 /////////////  TEST_CASE   /////////////////
 
 TEST_CASE("make layout", "[layout]") {
@@ -42,5 +78,44 @@ TEST_CASE("make layout", "[layout]") {
         html::Node n{html::Element{"p", {{"id", "test"}, {"class", "testclass"}}, {}}};
         css::Selector s{css::Type::ClassSelector, css::ClassSelector{"testclass"}};
         REQUIRE(selector_matches(n, s) == true);
+    }
+}
+
+TEST_CASE("to_style_node", "[style node]") {
+
+    SECTION("node1") {
+        html::Node n{html::Element{"p", {{"id", "test"}, {"class", "testclass"}}, {}}};
+        css::Selector s{css::Type::TypeSelector, css::TypeSelector{"p"}};
+        std::vector<css::Selector> selectors{s};
+        std::vector<css::Declaration> declarations{css::Declaration{"foo", "bar"},
+                                                   css::Declaration{"piyo", "piyopiyo"}};
+        std::vector<css::CSSRule> rules{css::CSSRule{selectors, declarations}};
+        css::Stylesheet sheet{rules};
+
+        std::unordered_map<std::string, std::string> properties{{"foo", "bar"},
+                                                                {"piyo", "piyopiyo"}};
+
+        auto expected =
+            std::make_shared<StyleNode>(StyleNode{html::NodeType::Element, {}, properties});
+        auto result = to_style_node(n, sheet);
+        REQUIRE_THAT(result, StyleNodeMatcher(expected));
+    }
+
+    SECTION("display node") {
+        html::Node n{html::Element{"p", {{"id", "test"}, {"class", "testclass"}}, {}}};
+
+        css::Selector s{
+            css::Type::AttributeSelector,
+            css::AttributeSelector{"p", css::AttributeSelectorOp::eq, "class", "testclass"}};
+
+        std::vector<css::Selector> selectors{s};
+        std::vector<css::Declaration> declarations{css::Declaration{"display", "none"},
+                                                   css::Declaration{"piyo", "piyopiyo"}};
+        std::vector<css::CSSRule> rules{css::CSSRule{selectors, declarations}};
+        css::Stylesheet sheet{rules};
+
+        auto expected = nullptr;
+        auto result = to_style_node(n, sheet);
+        REQUIRE(expected == result);
     }
 }
