40a34f68f198da5f1faf035a46cff74786e639d8
yk0112 <yosikou0726@gmail.com>
2024-01-31 01:16:36 +0900

    refactor: stylenodeなど変更

--- src/layout/layout.cpp
+++ src/layout/layout.cpp
@@ -1,122 +1,188 @@
-#include "../layout/layout.hpp"
-#include <boost/spirit/include/qi.hpp>
+#include "layout.hpp"
+#include <bit>
+#include <iostream>
 
 namespace layout {
 
-namespace qi = boost::spirit::qi;
+LayoutBox::LayoutBox(html::NodeType node,
+                     std::string text_data,
+                     render::propertyMap properties,
+                     render::DisplayType display_type,
+                     BoxType box_type) {
+    display_type = display_type;
+    box_type = box_type;
+    properties = properties;
 
-bool css_matches(const html::Node &node, const css::CSSRule &rule) {
-    return std::any_of(rule.selectors.begin(),
-                       rule.selectors.end(),
-                       [node](const css::Selector &s) {
-                           return layout::selector_matches(node, s);
-                       });
-};
-
-bool selector_matches(const html::Node &n, const css::Selector &s) {
-    switch (s.type) {
-
-    case css::Type::UniversalSelector:
-        return true;
-    case css::Type::TypeSelector: {
-        auto selector = std::get<1>(s.content);
-        if (std::holds_alternative<html::Element>(n) &&
-            selector.tag_name == std::get<1>(n).tag_name)
-            return true;
-        else
-            return false;
+    if (display_type == render::DisplayType::Text) {
+        text_node = text_render::constructText(this, text_data);
     }
-    case css::Type::AttributeSelector: {
-        if (std::holds_alternative<html::Element>(n))
-            return layout::AttributeSelector_matches(std::get<1>(n), s);
-        else
-            return false;
+    // if (box_type == BoxType::Img) {
+    // }
+}
+
+void LayoutBox::calculateWidth(Dimensions container) {
+    int paddingLeft = std::stoi(getStyleValue(constants::css_properties::PADDING_LEFT, "0"));
+    int paddingRight = std::stoi(getStyleValue(constants::css_properties::PADDING_RIGHT, "0"));
+    int borderLeft = std::stoi(getStyleValue(constants::css_properties::BORDER_LEFT_WIDTH, "0"));
+    int borderRight = std::stoi(getStyleValue(constants::css_properties::BORDER_RIGHT_WIDTH, "0"));
+
+    bool margin_left_isAuto = isAuto(constants::css_properties::MARGIN_LEFT);
+    int marginLeft;
+
+    bool margin_right_isAuto = isAuto(constants::css_properties::MARGIN_RIGHT);
+    int marginRight;
+
+    if (!margin_left_isAuto)
+        marginLeft = std::stoi(getStyleValue(constants::css_properties::MARGIN_LEFT, "0"));
+    if (!margin_right_isAuto)
+        marginRight = std::stoi(getStyleValue(constants::css_properties::MARGIN_RIGHT, "0"));
+
+    int width;
+    bool width_isAuto = isAuto(constants::css_properties::WIDTH);
+
+    if (display_type == render::DisplayType::Text) {
+        width = text_render::getTextWidth(this);
+    } else {
+        width = std::stoi(getStyleValue(constants::css_properties::WIDTH, "-1"));
     }
-    case css::Type::ClassSelector: {
-        auto selector = std::get<3>(s.content);
-        if (std::holds_alternative<html::Element>(n)) {
-            auto element = std::get<1>(n);
-            try {
-                return element.attributes.at("class") == selector.class_name;
-            } catch (const std::out_of_range &) {
-                return false;
-            }
+
+    int total = paddingLeft + paddingRight + std::max(marginLeft, 0) + std::max(marginRight, 0) +
+                borderLeft + borderRight + std::max(width, 0);
+
+    if (width != -1 && total > container.content.width) {
+        if (margin_left_isAuto) {
+            marginLeft = 0;
         }
-        return false;
-    }
-    default:
-        return false;
-    }
-};
-
-bool AttributeSelector_matches(const html::Element &e, const css::Selector &s) {
-
-    auto selector = std::get<2>(s.content);
-
-    if (!(selector.tag_name == e.tag_name))
-        return false;
-
-    if (selector.op == css::AttributeSelectorOp::eq && e.attributes.contains(selector.attribute)) {
-        try {
-            return e.attributes.at(selector.attribute) == selector.value;
-        } catch (const std::out_of_range &) {
-            return false;
+        if (margin_right_isAuto) {
+            marginRight = 0;
         }
     }
 
-    if (selector.op == css::AttributeSelectorOp::Contain &&
-        e.attributes.contains(selector.attribute)) {
-        auto attr = e.attributes.at(selector.attribute);
-        std::vector<std::string> values;
-        auto begin = attr.begin();
-        auto end = attr.end();
+    int underflow = container.content.width - total;
 
-        auto sucsess = qi::parse(begin, end, *(qi::char_ - ' ') % qi::space, values);
-
-        if (sucsess && begin == end)
-            return std::find(values.begin(), values.end(), selector.value) != values.end();
-        else
-            return false;
+    if (this->display_type == render::DisplayType::Text ||
+        this->display_type == render::DisplayType::Inline ||
+        (this->display_type == render::DisplayType::FlexChild && width_isAuto)) {
+        width = total;
     }
-    return false;
-};
 
-std::shared_ptr<StyleNode> to_style_node(const html::Node &node,
-                                         const css::Stylesheet &stylesheet) {
-    std::unordered_map<std::string, std::string> properties;
-
-    // Filterling, Cascading (order of appearance only)
-    for (auto &rule : stylesheet.rules) {
-        if (css_matches(node, rule)) {
-            for (auto &declaration : rule.declarations) {
-                properties[declaration.name] = declaration.value;
-            }
+    if (width_isAuto) {
+        if (margin_left_isAuto) {
+            marginLeft = 0;
         }
-    }
-
-    // defaulting is skipped
-
-    if (properties.find("display") != properties.end() && properties.at("display") == "none") {
-        return nullptr;
-    }
-
-    html::NodeType nodetype;
-    std::vector<std::shared_ptr<StyleNode>> children;
-
-    if (std::holds_alternative<html::Element>(node)) {
-        for (auto &child : std::get<1>(node).children) {
-            auto styled_child = to_style_node(child, stylesheet);
-            children.push_back(styled_child);
+        if (margin_right_isAuto) {
+            marginRight = 0;
         }
-        nodetype = html::NodeType::Element;
-    } else
-        nodetype = html::NodeType::Text;
+        if (underflow > 0) {
+            width = underflow;
+        } else {
+            // Width can't be negative. Adjust the right margin instead.
+            width = 0;
+            marginRight = marginRight + underflow;
+        }
+    } else if (margin_right_isAuto && margin_left_isAuto) {
+        marginRight = underflow / 2.0;
+        marginLeft = underflow / 2.0;
+    } else if (margin_left_isAuto) {
+        marginLeft = underflow;
+    } else if (margin_right_isAuto) {
+        marginRight = underflow;
+    } else {
+        marginRight = marginRight + underflow;
+    }
 
-    auto stylenode = std::make_shared<StyleNode>();
-    stylenode->node_type = nodetype;
-    stylenode->properties = properties;
-    stylenode->children = children;
+    dimensions.padding.left = paddingLeft;
+    dimensions.padding.right = paddingRight;
+    dimensions.margin.left = marginLeft;
+    dimensions.margin.right = marginRight;
+    dimensions.border.left = borderLeft;
+    dimensions.border.right = borderRight;
+    dimensions.content.width = width;
+}
 
-    return stylenode;
-};
+void LayoutBox::calculatePosition(Dimensions container,
+                                  int xCursor,
+                                  int yCursor,
+                                  bool shouldRenderBelow) {
+    int paddingTop = std::stoi(getStyleValue(constants::css_properties::PADDING_TOP, "0"));
+    int paddingBottom = std::stoi(getStyleValue(constants::css_properties::PADDING_BOTTOM, "0"));
+    int borderTop = std::stoi(getStyleValue(constants::css_properties::BORDER_TOP_WIDTH, "0"));
+    int borderBottom =
+        std::stoi(getStyleValue(constants::css_properties::BORDER_BOTTOM_WIDTH, "0"));
+
+    bool margin_top_isAuto = isAuto(constants::css_properties::MARGIN_LEFT);
+    int marginTop = 0;
+
+    bool margin_bottom_isAuto = isAuto(constants::css_properties::MARGIN_RIGHT);
+    int marginBottom = 0;
+
+    if (!margin_top_isAuto)
+        marginTop = std::stoi(getStyleValue(constants::css_properties::MARGIN_TOP, "0"));
+    if (!margin_bottom_isAuto)
+        marginBottom = std::stoi(getStyleValue(constants::css_properties::MARGIN_BOTTOM, "0"));
+
+    dimensions.padding.top = paddingTop;
+    dimensions.padding.bottom = paddingBottom;
+    dimensions.margin.top = marginTop;
+    dimensions.margin.bottom = marginBottom;
+    dimensions.border.top = borderTop;
+    dimensions.border.bottom = borderBottom;
+
+    int x = container.content.x + xCursor + dimensions.padding.left + dimensions.border.left +
+            dimensions.margin.left;
+    int y = yCursor + container.content.y + dimensions.padding.top + dimensions.border.top +
+            dimensions.margin.top;
+    dimensions.content.x = x;
+    dimensions.content.y = y;
+}
+
+void LayoutBox::layoutChildren(int parentWidth) {
+    int availableChildWidth;
+
+    if (display_type == render::DisplayType::Inline ||
+        display_type == render::DisplayType::FlexChild) {
+        availableChildWidth = parentWidth;
+    } else {
+        availableChildWidth = dimensions.content.width;
+    }
+
+    for (auto &child : children) {}
+}
+
+void LayoutBox::applyLayout(Dimensions container,
+                            int xCursor,
+                            int yCursor,
+                            bool shouldRenderBelow) {
+    calculateWidth(container);
+    calculatePosition(container, xCursor, yCursor, shouldRenderBelow);
+    layoutChildren(container.content.width - container.padding.left - container.padding.right);
+    setHeight();
+}
+
+BoxType ToBoxType(const std::string &tag_name) {
+    if (tag_name == "img") {
+        return BoxType::Img;
+    } else if (tag_name == "text") {
+        return BoxType::Text;
+    } else if (tag_name == "bullet") {
+        return BoxType::Bullet;
+    } else {
+        return BoxType::Shape;
+    }
+}
+
+std::unique_ptr<layout::LayoutBox> build_layout_tree(const render::StyledNode &styleTree) {
+    std::unique_ptr<layout::LayoutBox> layoutTree(new LayoutBox(styleTree.node_type,
+                                                                styleTree.text_data,
+                                                                styleTree.properties,
+                                                                styleTree.get_display_type(),
+                                                                ToBoxType(styleTree.tag_name)));
+
+    for (auto child : styleTree.children) {
+        std::unique_ptr<LayoutBox> childTree = build_layout_tree(*child);
+        layoutTree->addChild(std::move(childTree));
+    }
+    return layoutTree;
+}
+
 } // namespace layout
--- src/layout/layout.hpp
+++ src/layout/layout.hpp
@@ -1,20 +1,92 @@
-#include "../css/cssParser.hpp"
-#include "../html/htmlParser.hpp"
+#pragma once
+
+#include "../layout/text.h"
+#include "../render/render.hpp"
+#include <QLabel>
+#include <iostream>
+#include <string>
+#include <vector>
 
 namespace layout {
 
-struct StyleNode {
-    html::NodeType node_type;
-    std::vector<std::shared_ptr<StyleNode>> children;
-    std::unordered_map<std::string, std::string> properties;
+struct Rect {
+    int x = 0;
+    int y = 0;
+    int width = 0;
+    int height = 0;
 };
 
-bool css_matches(const html::Node &node, const css::CSSRule &rule);
+struct EdgeSizes {
+    int left = 0;
+    int right = 0;
+    int top = 0;
+    int bottom = 0;
+};
 
-bool selector_matches(const html::Node &n, const css::Selector &s);
+// box model of css
+struct Dimensions {
+    // Position of the content area relative to the document origin
+    Rect content;
 
-bool AttributeSelector_matches(const html::Element &e, const css::Selector &s);
+    // Surrounding edges
+    EdgeSizes padding;
+    EdgeSizes border;
+    EdgeSizes margin;
+};
 
-std::shared_ptr<StyleNode> to_style_node(const html::Node &node, const css::Stylesheet &stylesheet);
+enum class BoxType { Img, Text, Bullet, Shape };
+
+BoxType ToBoxType(const std::string &tag_name);
+
+struct LayoutBox {
+
+    LayoutBox(html::NodeType node,
+              std::string text_data,
+              render::propertyMap properties,
+              render::DisplayType display_type,
+              BoxType box_type);
+
+    void calculateWidth(Dimensions container);
+
+    void calculatePosition(Dimensions container, int xCursor, int yCursor, bool shouldRenderBelow);
+
+    void applyLayout(Dimensions container,
+                     int xCursor = 0,
+                     int yCursor = 0,
+                     bool shouldRenderBelow = true);
+
+    void layoutChildren(int parentWidth);
+
+    void addChild(std::unique_ptr<LayoutBox> child) {
+        children.push_back(std::move(child));
+    }
+
+    std::string getStyleValue(const std::string &property, const std::string defaultValue) const {
+        return render::get_property_value(properties, property, defaultValue);
+    }
+
+    bool isAuto(const std::string &property) {
+        if (properties.find(property) != properties.end() && properties.at(property) == "auto")
+            return true;
+        return false;
+    }
+
+    const QLabel &get_text_node() {
+        return *text_node;
+    }
+
+  private:
+    html::NodeType node;
+    render::propertyMap properties;
+    render::DisplayType display_type;
+    std::vector<std::unique_ptr<LayoutBox>> children;
+    Dimensions dimensions;
+    BoxType box_type;
+    std::unique_ptr<QLabel> text_node;
+};
+
+std::unique_ptr<LayoutBox> build_layout_tree(const render::StyledNode &styleTree);
+
+std::unique_ptr<LayoutBox> build_layout_tree(const render::StyledNode &styleTree);
 
 } // namespace layout
